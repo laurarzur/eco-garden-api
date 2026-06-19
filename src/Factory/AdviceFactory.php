90b9eff2ef606c716cd22ba2c02d11430f1b3126
laurarzur <laura.arzur@gmail.com>
2026-06-19 08:57:55 +0200

    conseils: création de l'entité Advice et des fixtures

---
+++ src/Factory/AdviceFactory.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Advice;
+use App\Repository\AdviceRepository;
+use Doctrine\ORM\EntityRepository;
+use Zenstruck\Foundry\Persistence\PersistentObjectFactory;
+use Zenstruck\Foundry\Persistence\Proxy;
+use Zenstruck\Foundry\Persistence\ProxyRepositoryDecorator;
+
+/**
+ * @extends PersistentObjectFactory<Advice>
+ */
+final class AdviceFactory extends PersistentObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct() {}
+
+    #[\Override]
+    public static function class(): string
+    {
+        return Advice::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    #[\Override]
+    protected function defaults(): array|callable
+    {
+        return [
+            'content' => self::faker()->text(),
+            'month' => self::faker()->numberBetween(1, 12),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    #[\Override]
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Advice $advice): void {})
+        ;
+    }
+}
