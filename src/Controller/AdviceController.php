8687e8f56817ece6af2db85bfa9c96ddf5ee899f
laurarzur <laura.arzur@gmail.com>
2026-06-19 10:11:16 +0200

    conseils: création de la route qui récupère les conseils du mois en cours

---
+++ src/Controller/AdviceController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AdviceRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+final class AdviceController extends AbstractController
+{
+    #[Route('/api/conseil', name: 'currentAdvice', methods: ['GET'])]
+    public function getCurrentAdvice(AdviceRepository $repo, SerializerInterface $serializer): JsonResponse
+    {
+        $currentMonth = date('n');
+        $adviceList = $repo->findByMonth($currentMonth);
+        $jsonAdviceList = $serializer->serialize($adviceList, 'json');
+        return new JsonResponse($jsonAdviceList, Response::HTTP_OK, [], true);
+    }
+}
--- src/Repository/AdviceRepository.php
+++ src/Repository/AdviceRepository.php
@@ -16,28 +16,27 @@ class AdviceRepository extends ServiceEntityRepository
         parent::__construct($registry, Advice::class);
     }
 
-//    /**
-//     * @return Advice[] Returns an array of Advice objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('a.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /**
+     * @return Advice[] Returns an array of Advice objects
+     */
+    public function findByMonth(int $month): array
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.month = :month')
+            ->setParameter('month', $month)
+            ->orderBy('a.id', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 
-//    public function findOneBySomeField($value): ?Advice
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?Advice
+    //    {
+    //        return $this->createQueryBuilder('a')
+    //            ->andWhere('a.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
